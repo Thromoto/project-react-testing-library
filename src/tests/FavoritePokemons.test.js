bbb312a3b03f095d66182052c2dc09eeabd426e5
Thiago Omoto <thi.omoto@gmail.com>
2022-10-19 17:44:08 -0300

    requisito 3 done

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../pages';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    screen.logTestingPlaygroundURL();
+    const msgTela = screen.getByText(/no favorite pokemon found/i);
+    expect(msgTela).toBeInTheDocument();
+  });
+});
